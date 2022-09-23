edbd9416cacc32e053afc12c2b0b300462c5feb7
meixea <mixael@bk.ru>
2022-09-23 15:58:46 +0600

    Job done.

--- src/main/java/com/mycompany/problem/DataStructure.java
+++ src/main/java/com/mycompany/problem/DataStructure.java
@@ -1,34 +1,3 @@
-/*
- * Copyright (c) 2013, Oracle and/or its affiliates. All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- *   - Redistributions of source code must retain the above copyright
- *     notice, this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright
- *     notice, this list of conditions and the following disclaimer in the
- *     documentation and/or other materials provided with the distribution.
- *
- *   - Neither the name of Oracle or the names of its
- *     contributors may be used to endorse or promote products derived
- *     from this software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
- * IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
- * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
- * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
- * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
- * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
- * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
 package com.mycompany.problem;
  
 public class DataStructure {
@@ -53,6 +22,12 @@ public class DataStructure {
         }
         System.out.println();
     }
+    public void print(DataStructureIterator iterator) {
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        System.out.println();
+    }
     
     interface DataStructureIterator extends java.util.Iterator<Integer> { } 
 
@@ -64,6 +39,11 @@ public class DataStructure {
         // Start stepping through the array from the beginning
         private int nextIndex = 0;
         
+        private EvenIterator() {}
+        private EvenIterator(int start_index) {
+            nextIndex = start_index;
+        }
+        
         public boolean hasNext() {
             
             // Check if the current element is the last in the array
@@ -73,7 +53,8 @@ public class DataStructure {
         public Integer next() {
             
             // Record a value of an even index of the array
-            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+            //Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
+            Integer retValue = arrayOfInts[nextIndex];
             
             // Get the next even element
             nextIndex += 2;
@@ -81,11 +62,29 @@ public class DataStructure {
         }
     }
     
+    public void print(java.util.function.Function<Integer, Boolean> iterator2){
+        for (Integer i = 0; i < SIZE; i++) {
+            if ( iterator2.apply(i) )
+                System.out.print(arrayOfInts[i] + " ");
+        }
+        System.out.println();
+    }
+    
+    public static Boolean isEvenIndex(Integer i){
+        return i % 2 == 0;
+    }
+    public static Boolean isOddIndex(Integer i){
+        return i % 2 == 1;
+    }
+    
     public static void main(String s[]) {
         
         // Fill the array with integer values and print out only
         // values of even indices
         DataStructure ds = new DataStructure();
-        ds.printEven();
+        //ds.print((i) -> { return i % 2 == 1; });
+        //ds.print(ds.new EvenIterator(1));
+        ds.print(DataStructure::isEvenIndex);
+        ds.print(DataStructure::isOddIndex);
     }
 }
